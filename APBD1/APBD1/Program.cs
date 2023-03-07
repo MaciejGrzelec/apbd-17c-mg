94e12b06894ca960f645dde0ba36ac0370e1640b
gitMaciek86 <maciej.and.grzelec@gmail.com>
2023-03-07 23:09:08 +0100

    APBD Praca domowa no. 1

---
+++ APBD1/APBD1/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System.Net;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        if (args.Length < 1) throw new ArgumentNullException();
+        var url = args[0];
+
+        if (!IsValidUrl(url)) throw new ArgumentException();
+
+        var httpClient = new HttpClient();
+
+        var regex = new Regex(@"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+");
+
+        var response = await httpClient.GetAsync(url);
+
+        HttpStatusCode statusCode = response.StatusCode;
+        if((int)statusCode<200 || (int)statusCode > 299) {
+            throw new Exception("Błąd w czasie pobierania strony");
+        };
+
+        string content = await response.Content.ReadAsStringAsync();
+
+        MatchCollection result = regex.Matches(content);
+
+        Hashtable hashtable = new Hashtable();
+
+        string foundmatch;
+
+        foreach (Match match in result)
+        {
+            foundmatch = match.ToString();
+            if (hashtable.Contains(foundmatch) == false)
+                hashtable.Add(foundmatch, string.Empty);
+        }
+
+        if(hashtable.Count == 0) throw new Exception("Nie znaleziono adresów email");
+
+        foreach (DictionaryEntry element in hashtable)
+        {
+            Console.WriteLine(element.Key);
+        }
+    }
+
+    public static bool IsValidUrl(string url)
+    {
+        Uri? uriResult;
+        var test1= Uri.TryCreate(url, UriKind.Absolute, out uriResult) &&
+                (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+        var test2= Uri.IsWellFormedUriString(url, UriKind.Absolute);
+
+        return test1 && test2;
+    }
+}
---
+++ MoshCodes/MoshCodes/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int int1 = 1;
+        int int2;
+
+        string str1 = "1";
+        string str2 = "true";
+        
+        bool bool1 = Convert.ToBoolean(str2);
+
+        int1 = Convert.ToInt32(str1);
+        int2 = int.Parse(str1);
+
+        var var1 = int2;
+
+        var student1 = new Student();
+
+        Console.WriteLine(student1.id); 
+        
+        int[] numbers = new int[3];
+        Console.WriteLine(numbers[0]);
+        //placeholdery
+        string name = string.Format("{0} {1}", str1, str2);
+        Console.WriteLine(name);
+
+        string list = string.Join(", ", numbers);
+        Console.WriteLine(list);
+        string str1CSharp;
+        String strDotNet;
+
+
+        try
+        {
+
+        } catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+
+    }
+}
+
+public class Student
+{
+    public int id = 1;
+}
